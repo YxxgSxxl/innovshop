1229da0d0dc6a93d34438a32a3a4a3ea7687f0fe
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-09-18 11:51:59 +0200

    feat: added knp_paginator bundle to symfony + added config

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
