30dba1c126df2f9f980205ab2ecdbb29a7362c24
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-09-25 11:12:34 +0200

    up

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
